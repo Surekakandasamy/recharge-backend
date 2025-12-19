015ebaa796ca760c61c847840e837536b8375658
Surekakandasamy <surekasureka978@gmail.com>
2025-12-19 15:55:30 +0530

    Delete backend directory

--- backend/src/config/env.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  NODE_ENV: process.env.NODE_ENV || 'development',
-  PORT: process.env.PORT,
-  MONGODB_URI: process.env.MONGODB_URI,
-  JWT_SECRET: process.env.JWT_SECRET,
-  JWT_EXPIRE: process.env.JWT_EXPIRE || '7d',
-  CORS_ORIGIN: process.env.CORS_ORIGIN || 'https://recharge-frontend-1.onrender.com'
-};
